447eae66c4b3514c1a8f223fe7650fc71aa0de70
Joyce Matos <joycematos@Joyces-Air.home>
2017-07-16 17:56:45 -0400

    Add view controllers

---
+++ BookSwap/LocationView.swift
@@ -0,0 +1,21 @@
+//
+//  LocationView.swift
+//  BookSwap
+//
+//  Created by Joyce Matos on 7/16/17.
+//  Copyright © 2017 Joyce Matos. All rights reserved.
+//
+
+import UIKit
+
+class LocationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
