db47186ab19274718859c07b3bded3c53ca79ab5
Joyce Matos <joycematos@Joyces-Air.home>
2017-07-20 14:31:12 -0400

    Work on collectionView

---
+++ BookSwap/BookCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookCollectionViewCell.swift
+//  BookSwap
+//
+//  Created by Joyce Matos on 7/20/17.
+//  Copyright © 2017 Joyce Matos. All rights reserved.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- BookSwap/HomeView.swift
+++ BookSwap/HomeView.swift
@@ -13,5 +13,49 @@ class HomeView: UIView {
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var collectionView: UICollectionView!
     
+    let reuseIdentifier = "bookCell"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed("HomeView", owner: self, options: nil)
+        collectionView.register(BookCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        addSubview(contentView)
+        
+        setupCollectionView()
+    }
+    
+    func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+}
+
+extension HomeView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! BookCollectionViewCell
+        
+        print("Hello in cell!")
+        
+        return cell
+    }
+   
 }
