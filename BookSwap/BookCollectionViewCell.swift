643bc2f63d212269b10ee6f7aa1765168543f413
Joyce Matos <joycematos@Joyces-Air.home>
2017-07-28 21:22:03 -0400

    Load collectionViewCell

--- BookSwap/BookCollectionViewCell.swift
+++ BookSwap/BookCollectionViewCell.swift
@@ -2,16 +2,21 @@
 //  BookCollectionViewCell.swift
 //  BookSwap
 //
-//  Created by Joyce Matos on 7/20/17.
+//  Created by Joyce Matos on 7/28/17.
 //  Copyright © 2017 Joyce Matos. All rights reserved.
 //
 
 import UIKit
 
 class BookCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
+
     }
+    
 
 }
--- BookSwap/HomeView.swift
+++ BookSwap/HomeView.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class HomeView: UIView {
 
-    @IBOutlet weak var contentView: UIView!
+    @IBOutlet var contentView: UIView!
     @IBOutlet weak var collectionView: UICollectionView!
     
+    // TODO: - Move this info out of the view
+    fileprivate let leftAndRightPadding: CGFloat = 32.0
+    fileprivate let itemsPerRow: CGFloat = 3.0
+    fileprivate let heightAdjustment: CGFloat = 64.0
+    fileprivate let sectionInsets = UIEdgeInsets(top: 8.0, left: 8.0, bottom: 8.0, right: 8.0)
+
     let reuseIdentifier = "bookCell"
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
-    }
+        
+          }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -27,7 +34,8 @@ class HomeView: UIView {
     
     func commonInit() {
         Bundle.main.loadNibNamed(Nib.home, owner: self, options: nil)
-        collectionView.register(BookCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+       // collectionView.register(BookCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.register(UINib(nibName: "BookCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
         addSubview(contentView)
         
         setupCollectionView()
@@ -40,22 +48,49 @@ class HomeView: UIView {
     
 }
 
-extension HomeView: UICollectionViewDelegate, UICollectionViewDataSource {
+extension HomeView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 9
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! BookCollectionViewCell
-        
-        cell.backgroundColor = UIColor.blue
-        
+       // let cell = Bundle.main.loadNibNamed("BookCollectionViewCell", owner: self, options: nil)?.first as! BookCollectionViewCell
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bookCell", for: indexPath) as! BookCollectionViewCell
+                
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (collectionView.frame.width - leftAndRightPadding) / itemsPerRow
+        
+        return CGSize(width: width, height: width + heightAdjustment)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return sectionInsets
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+    }
+    
+    
+    
    
 }
